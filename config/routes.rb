196713d88b8e6643552301d4d708f77911b39e81
S SAI TANUSH <saitanush789@gmail.com>
2025-02-13 14:09:31 +0530

    feat: added authentication routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,12 @@ Rails.application.routes.draw do
   get "/play", to: "pages#play"
   get "/moments", to: "pages#moments"
   get "/profile", to: "users#profile"
+
+    # Authentication routes
+    get "/login", to: "sessions#new"
+    post "/login", to: "sessions#create"
+    delete "/logout", to: "sessions#destroy"
+  
+    get "/signup", to: "registrations#new"
+    post "/signup", to: "registrations#create"
 end
