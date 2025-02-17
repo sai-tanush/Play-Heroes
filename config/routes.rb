cc7d97ad1804a5e78c0a0f375fc66ea6a1a938c4
S SAI TANUSH <saitanush789@gmail.com>
2025-02-17 10:12:49 +0530

    refactor: removed unnecessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   get "home/index"
   root "home#index"
-  get "/play", to: "pages#play"
-  get "/moments", to: "pages#moments"
-  get "/profile", to: "users#profile"
 
-    # Authentication routes
-    get "/login", to: "sessions#new"
-    post "/login", to: "sessions#create"
-    delete "/logout", to: "sessions#destroy"
-  
-    get "/signup", to: "registrations#new"
-    post "/signup", to: "registrations#create"
+  authenticate :user do
+    get "/play", to: "pages#play"
+    get "/moments", to: "pages#moments"
+    get "/profile", to: "users#profile"
+  end
+
 end
