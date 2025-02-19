75199cd9c1f5680b8cd03b71b42ba3a9dc7bc81e
S SAI TANUSH <saitanush789@gmail.com>
2025-02-19 12:57:11 +0530

    feat: Posts db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_19_052414) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_19_072526) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -40,6 +40,15 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_19_052414) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.text "description"
+    t.string "media_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "sports", force: :cascade do |t|
@@ -77,6 +86,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_19_052414) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "posts", "users"
   add_foreign_key "user_sports", "sports", column: "sports_id"
   add_foreign_key "user_sports", "users"
 end
