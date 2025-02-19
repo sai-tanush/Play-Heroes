ac36ca315f5dd85895fa945f52e161374e89a932
S SAI TANUSH <saitanush789@gmail.com>
2025-02-19 12:39:27 +0530

    dev: added new db migration for user and sport table

--- db/migrate/20250217090226_create_join_table_users_sports.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableUsersSports < ActiveRecord::Migration[8.0]
-  def change
-    create_join_table :users, :sports do |t|
-      # t.index [:user_id, :sport_id]
-      # t.index [:sport_id, :user_id]
-    end
-  end
-end
