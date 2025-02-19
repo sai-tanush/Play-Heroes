6c70bf0cf8753f5ec5c2c56d8a9f7385592a1dbe
S SAI TANUSH <saitanush789@gmail.com>
2025-02-19 14:09:10 +0530

    feat: updated posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,31 +1,51 @@
 class PostsController < ApplicationController
-    before_action :authenticate_user!
     before_action :set_post, only: [:show, :edit, :update, :destroy]
+    before_action :authenticate_user!
+    before_action :authorize_user, only: [:edit, :update, :destroy]
   
     def index
-      @posts = Post.all.order(created_at: :desc)
+      @posts = Post.includes(:user).order(created_at: :desc)
     end
   
     def show
     end
   
     def new
-      @post = Post.new
+      @post = current_user.posts.build
     end
   
     def create
       @post = current_user.posts.build(post_params)
       if @post.save
-        redirect_to @post, notice: 'Post created successfully.'
+        redirect_to moments_path, notice: "Post created successfully."
       else
         render :new, status: :unprocessable_entity
       end
     end
   
-    def destroy
-      @post.destroy
-      redirect_to posts_path, notice: "Post deleted successfully."
+    def edit
     end
+  
+    def update
+      if @post.update(post_params)
+        redirect_to moments_path, notice: "Post updated successfully."
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
+  
+    def destroy
+        @post = Post.find(params[:id])
+      
+        if @post.user == current_user
+          @post.destroy
+          redirect_to posts_path, notice: "Post deleted successfully."
+        else
+          redirect_to posts_path, alert: "You are not authorized to delete this post."
+        end
+    end
+      
+      
   
     private
   
@@ -35,6 +55,12 @@ class PostsController < ApplicationController
   
     def post_params
       params.require(:post).permit(:description, :media)
+    end
+  
+    def authorize_user
+      unless @post.user == current_user
+        redirect_to moments_path, alert: "You can only modify your own posts."
+      end
     end
   end
   
