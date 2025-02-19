7bb8903f2785acdac58cde13ce05134d1c59893b
S SAI TANUSH <saitanush789@gmail.com>
2025-02-19 12:59:44 +0530

    feat: added some validations for description and media_url for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :user
+  validates :description, presence: true
+  validates :media_url, format: { with: URI::regexp(%w[http https]), message: "must be a valid URL" }, allow_blank: true
 end
