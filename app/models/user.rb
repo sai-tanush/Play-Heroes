1a72c75816e28c1eedad3bd08abf3082f4c77980
S SAI TANUSH <saitanush789@gmail.com>
2025-02-19 12:58:41 +0530

    feat: added user associations for posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  # Association to sports (many-to-many relationship)
+  # Associations
   has_and_belongs_to_many :sports
-
   has_one_attached :profile_picture
+  has_many :posts, dependent: :destroy
 end
