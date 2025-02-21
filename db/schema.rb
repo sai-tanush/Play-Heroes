2b601ebfdc3d207cba496b3e30259adbe2341233
S Sai Tanush <saitanush789@gmail.com>
2025-02-21 08:44:59 +0530

    feat: post likes table changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_20_071502) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_21_031117) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -39,8 +39,17 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_20_071502) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "post_likes", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_post_likes_on_post_id"
+    t.index ["user_id"], name: "index_post_likes_on_user_id"
+  end
+
   create_table "posts", force: :cascade do |t|
-    t.bigint "user_id", null: false
+    t.integer "user_id", null: false
     t.text "description"
     t.string "media_url"
     t.datetime "created_at", null: false
@@ -66,8 +75,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_20_071502) do
   end
 
   create_table "user_sports", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "sports_id", null: false
+    t.integer "user_id", null: false
+    t.integer "sports_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["sports_id"], name: "index_user_sports_on_sports_id"
@@ -94,6 +103,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_20_071502) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "post_likes", "posts"
+  add_foreign_key "post_likes", "users"
   add_foreign_key "posts", "users"
   add_foreign_key "sport_sessions", "sports"
   add_foreign_key "sport_sessions", "users"
