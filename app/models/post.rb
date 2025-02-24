a638029885d48dee5c868fb5ab556abd0649f8ae
S SAI TANUSH <saitanush789@gmail.com>
2025-02-24 12:50:41 +0530

    feat: upated my post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,5 @@ class Post < ApplicationRecord
   validates :description, presence: true
   has_one_attached :media
   validates :media_url, format: { with: URI::regexp(%w[http https]), message: "must be a valid URL" }, allow_blank: true
+  has_many :comments, dependent: :destroy
 end
