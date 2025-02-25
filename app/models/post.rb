45ce65a7c6a0638b9b2fc886c0c52608c0e3dc71
S SAI TANUSH <saitanush789@gmail.com>
2025-02-25 08:29:30 +0530

    feat: made changes in Post model to have multiple media

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   has_many :post_likes, dependent: :destroy
   belongs_to :user
   validates :description, presence: true
-  has_one_attached :media
+  has_many_attached :media
   validates :media_url, format: { with: URI::regexp(%w[http https]), message: "must be a valid URL" }, allow_blank: true
   has_many :comments, dependent: :destroy
 end
