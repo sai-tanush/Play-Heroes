4f9a01735276cae1d4e575593b0144e6a8cbc0fe
S SAI TANUSH <saitanush789@gmail.com>
2025-02-28 13:22:34 +0530

    feat: updated joinrequest migration file

--- db/migrate/20250228071603_create_join_requests.rb
+++ db/migrate/20250228071603_create_join_requests.rb
@@ -3,10 +3,11 @@ class CreateJoinRequests < ActiveRecord::Migration[8.0]
     create_table :join_requests do |t|
       t.references :user, null: false, foreign_key: true
       t.references :play_event, null: false, foreign_key: true
-      t.string :status
+      t.string :status, default: 'pending'
       t.text :message
-
       t.timestamps
+
+      t.index [:user_id, :play_event_id], unique: true
     end
   end
 end
