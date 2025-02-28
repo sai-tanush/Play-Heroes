fcafeb5d50030f93c50c26635fb9b7e7ba431a81
S SAI TANUSH <saitanush789@gmail.com>
2025-02-28 12:48:14 +0530

    feat: updated play_events controller

--- app/controllers/play_events_controller.rb
+++ app/controllers/play_events_controller.rb
@@ -1,31 +1,42 @@
 class PlayEventsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show] # Require login except index and show
-  before_action :set_play_event, only: [:show, :edit, :update, :destroy, :join]
-
+  before_action :set_play_event, only: [:show, :edit, :update, :destroy, :join, :leave]
+  
   def index
-    @play_events = PlayEvent.all
+    @play_events = PlayEvent.where('event_start_time >= ?', DateTime.now)
+                           .order(event_start_time: :asc)
+                           
+    if user_signed_in?
+      @joined_events = current_user.joined_events.order(event_start_time: :asc)
+    end
   end
-
+  
   def show
+    @participants = @play_event.participants
   end
-
+  
   def new
     @play_event = PlayEvent.new
   end
-
+  
   def create
     @play_event = PlayEvent.new(play_event_params)
     @play_event.host = current_user
+    
     if @play_event.save
       redirect_to @play_event, notice: 'Event created successfully.'
     else
       render :new
     end
   end
-
+  
   def edit
+    # Check if current user is the host
+    unless @play_event.host == current_user
+      redirect_to play_events_path, alert: 'You can only edit events you are hosting.'
+    end
   end
-
+  
   def update
     if @play_event.update(play_event_params)
       redirect_to @play_event, notice: 'Event updated successfully.'
@@ -33,24 +44,51 @@ class PlayEventsController < ApplicationController
       render :edit
     end
   end
-
+  
   def destroy
-    @play_event.destroy
-    redirect_to play_events_url, notice: 'Event deleted successfully.'
+    # Check if current user is the host
+    if @play_event.host == current_user
+      @play_event.destroy
+      redirect_to play_events_url, notice: 'Event deleted successfully.'
+    else
+      redirect_to play_events_path, alert: 'You can only delete events you are hosting.'
+    end
   end
-
+  
   def join
-    EventParticipant.create(play_event: @play_event, user: current_user)
-    redirect_to @play_event, notice: 'You have joined the event.'
+    # Check if user is already a participant
+    if @play_event.participants.include?(current_user)
+      redirect_to play_events_path, notice: 'You are already joined to this event.'
+    # Check if event is full
+    elsif @play_event.event_capacity.present? && @play_event.participants.count >= @play_event.event_capacity
+      redirect_to play_events_path, alert: 'This event is already full.'
+    else
+      EventParticipant.create(play_event: @play_event, user: current_user)
+      redirect_to play_events_path, notice: 'You have joined the event.'
+    end
   end
-
+  
+  def leave
+    # Find the participant record and destroy it
+    participant = EventParticipant.find_by(play_event: @play_event, user: current_user)
+    
+    if participant
+      participant.destroy
+      redirect_to play_events_path, notice: 'You have left the event.'
+    else
+      redirect_to play_events_path, alert: 'You are not a participant in this event.'
+    end
+  end
+  
   private
-
+  
   def set_play_event
     @play_event = PlayEvent.find(params[:id])
   end
-
+  
   def play_event_params
-    params.require(:play_event).permit(:sport_id, :sport_type, :event_location, :event_category, :event_instructions, :event_date, :event_start_time, :event_end_time, :event_capacity)
+    params.require(:play_event).permit(:sport_id, :sport_type, :event_location, :event_category, 
+                                      :event_instructions, :event_start_time, :event_end_time, 
+                                      :event_capacity)
   end
 end
